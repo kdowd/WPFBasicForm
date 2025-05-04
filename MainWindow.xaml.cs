d2ff7974aa984be689a9af9d4def4aeb301c0e7d
kdowd <kevin.dowd@gmail.com>
2025-05-04 22:18:24 +1200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BasicForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void onMakeUpdated(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox temp)
+            {
+                var x = temp.SelectedIndex;
+                var y = temp.SelectedValue;
+
+                if (temp.SelectedItem is ComboBoxItem z)
+                {
+                    MessageBox.Show(z.Content.ToString());
+                }
+            }
+        }
+    }
+}
